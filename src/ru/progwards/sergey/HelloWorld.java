3dd8eff6aec90fb1d18d6e1b9e693b7eb4f15596
Sergey <epishkow_sergey@mail.ru>
2019-11-13 17:27:30 +0300

    HelloWorld start full

--- src/ru/progwards/sergey/HelloWorld.java
+++ src/ru/progwards/sergey/HelloWorld.java
@@ -1,7 +1,19 @@
 package ru.progwards.sergey;
 
 public class HelloWorld {
+    static void println(String str) {
+        System.out.println(str);
+    }
+
+    static int addition(int x, int y) {
+        return x + y;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+        println("Привет, Мир!");
+        println("Мир, ты слышишь меня?");
+        System.out.print("Я знаю, что 5 + 7 = ");
+        System.out.println(addition(5, 7));
+
     }
 }
