c2064f026e9bb00da2f1c82c50786bc178be727a
Sergey <epishkow_sergey@mail.ru>
2019-12-01 21:33:21 +0300

    H6 Домашнее задание. Задача 1.

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -6,12 +6,27 @@ public class Eratosthenes {
     private boolean[] sieve;
 
     public Eratosthenes(int N) {
-        this.sieve = new boolean[N];
+        this.sieve = new boolean[N + 1];
         Arrays.fill(sieve, true);
         shift();
     }
 
     private void shift() {
-//       Неделю думал, ни фига не понял что конкретно этом метод должен делать!!!
+        for (int i = 2; i < sieve.length; i++) {
+            for (int j = i +1; j < sieve.length; j++) {
+                if (j % i == 0) {
+                    sieve[j] = false;
+                }
+            }
+        }
+    }
+
+    public boolean isSimple(int n) {
+        return sieve[n];
+    }
+
+    public static void main(String[] args) {
+        Eratosthenes eratosthenes = new Eratosthenes(1000);
+        System.out.println(eratosthenes.isSimple(5));
     }
 }
