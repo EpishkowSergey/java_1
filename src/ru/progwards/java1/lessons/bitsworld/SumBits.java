926f071b24a090c5c9b8a3a64abceaf8f747f2fc
Sergey <epishkow_sergey@mail.ru>
2019-12-01 21:54:02 +0300

    H5 Домашнее задание. Задача 1. Класс SumBits. Переделал с циклом for.

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,17 +3,17 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
     public static int sumBits(byte value) {
         int result = 0;
-        while (value > 0) {
+        for (int i = 0; i < 8; i++) {
             if ((value & 1) == 1) {
                 result ++;
             }
-            value >>=1;
+            value >>= 1;
         }
 
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b0100101));
+        System.out.println(sumBits((byte) 0b1100101));
     }
 }
