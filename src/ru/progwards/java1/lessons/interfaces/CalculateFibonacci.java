b0b2260e36bde79189aa43cfa5b770c4130dccab
Sergey <epishkow_sergey@mail.ru>
2019-12-05 16:16:29 +0300

    H7 Домашнее задание. Задача 2. Переделал fiboNumber.

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -1,33 +1,44 @@
 package ru.progwards.java1.lessons.interfaces;
 
+import org.checkerframework.checker.units.qual.C;
+
 public class CalculateFibonacci {
-    static CacheInfo lastFibo;
+    static CacheInfo lastFibo = new CacheInfo();
+
 
     public static int fiboNumber(int n) {
-        int fibo = 0;
         if (n == CalculateFibonacci.lastFibo.n) {
-            fibo = CalculateFibonacci.lastFibo.fibo;
+            return CalculateFibonacci.lastFibo.fibo;
         }
-        else {
-            if (n == 1 || n == 2) {
-                return 1;
-            }
 
-            int a = 1;
-            int b = 1;
-            int f = 0;
-            for (int i = 2; i < n; i++) {
-                f = a + b;
-                a = b;
-                b = f;
-            }
-            CalculateFibonacci.lastFibo.fibo = f;
+        if (n == 1 || n == 2) {
+            return 1;
         }
-        return fibo;
+
+        int a = 1;
+        int b = 1;
+        int f = 0;
+        for (int i = 2; i < n; i++) {
+            f = a + b;
+            a = b;
+            b = f;
+        }
+
+        CalculateFibonacci.lastFibo.n = n;
+        CalculateFibonacci.lastFibo.fibo = f;
+
+        return f;
     }
 
     static class CacheInfo {
         int n;
         int fibo;
+    }
+
+    public static void main(String[] args) {
+        CalculateFibonacci calculateFibonacci = new CalculateFibonacci();
+        System.out.println(CalculateFibonacci.lastFibo.n);
+        System.out.println(CalculateFibonacci.lastFibo.fibo);
+        System.out.println(CalculateFibonacci.fiboNumber(10));
     }
 }
