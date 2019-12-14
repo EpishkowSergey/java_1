8bca5a29bca0bd064ac8e8e5726a7e203475e157
Sergey <epishkow_sergey@mail.ru>
2019-12-15 01:36:54 +0300

    H8 Домашнее задание. Задача 3. Переделал.

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -75,7 +75,7 @@ public class ArrayInteger {
             for (int i = 0; i < num.digits.length; i++) {
                 byte number = (byte) (digits[i] + num.digits[i]);
 
-                if (number >= 10 & digits[num.digits.length] == 9) {
+                if (number >= 10 & digits[num.digits.length] == 9 & digits.length == num.digits.length + 1) {
                     for (int j = 0; j < digits.length; j++) {
                         digits[j] = 0;
                     }
@@ -85,9 +85,20 @@ public class ArrayInteger {
                 if (number >= 10) {
                     digits[i] = (byte) (number % 10);
                     digits[i + 1] = (byte) (digits[i + 1] + (number / 10));
-
                 } else {
                     digits[i] = number;
+                }
+            }
+
+            for (int i = 0; i < digits.length; i++) {
+                if (digits[i] > 9 & i == digits.length - 1) {
+                    for (int j = 0; j < digits.length; j++) {
+                        digits[i] = 0;
+                    }
+                }
+                else if (digits[i] > 9 ) {
+                    digits[i + 1] = (byte) (digits[i + 1] + digits[i] / 10);
+                    digits[i] = (byte) (digits[i] % 10);
                 }
             }
             result = true;
@@ -97,14 +108,14 @@ public class ArrayInteger {
 
 
     public static void main(String[] args) {
-        ArrayInteger arrayInteger = new ArrayInteger(5);
+        ArrayInteger arrayInteger = new ArrayInteger(8);
 //        System.out.println(Arrays.toString(arrayInteger.digits));
-        arrayInteger.fromInt(new BigInteger("10485"));
+        arrayInteger.fromInt(new BigInteger("15980465"));
        System.out.println(Arrays.toString(arrayInteger.digits));
        System.out.println(arrayInteger.toInt());
 
-        ArrayInteger arrayInteger1 = new ArrayInteger(6);
-        arrayInteger1.fromInt(new BigInteger("675840"));
+        ArrayInteger arrayInteger1 = new ArrayInteger(5);
+        arrayInteger1.fromInt(new BigInteger("69268"));
 
         System.out.println(arrayInteger.add(arrayInteger1));
         System.out.println(Arrays.toString(arrayInteger.digits));
