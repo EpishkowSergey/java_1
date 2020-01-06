3067404f2b9d9043b397f2c9d1b89940b5d5880e
Sergey <epishkow_sergey@mail.ru>
2020-01-06 23:16:30 +0300

    H9 Домашнее задание. Задача 1.

---
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -0,0 +1,31 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class LineCount {
+    public static int calcEmpty(String fileName) {
+        int i = 0;
+        try {
+            FileReader reader = new FileReader(fileName);
+            try {
+                Scanner scanner = new Scanner(reader);
+                while (scanner.hasNextLine()) {
+                    String strFromFile = scanner.nextLine();
+                    if (strFromFile.equals("\n")) {
+                        i++;
+                    }
+                }
+            }
+            finally {
+                reader.close();
+            }
+
+        }
+        catch (Exception e) {
+            return -1;
+        }
+
+        return i;
+    }
+}
