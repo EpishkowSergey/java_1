c0f59704f896266d6417b101c4d8208caadabcd6
Sergey <epishkow_sergey@mail.ru>
2020-06-24 22:48:05 +0300

    H11 Домашнее задание. Задача 2.

---
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -0,0 +1,77 @@
+package ru.progwards.java1.lessons.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Finder {
+    public static Collection<Integer> findMinSumPair(Collection<Integer> numbers) {
+        ArrayList<Integer> arrayList = (ArrayList<Integer>) numbers;
+
+        List<Integer> result = new ArrayList<>();
+        result.add(arrayList.get(0));
+        result.add(arrayList.get(1));
+        Integer sum = arrayList.get(0) + arrayList.get(1);
+
+        for (int i = 1; i < arrayList.size() - 1; i++) {
+            if (arrayList.get(i) + arrayList.get(i + 1) < sum) {
+                sum = arrayList.get(i) + arrayList.get(i + 1);
+                result.set(0, arrayList.get(i));
+                result.set(1, arrayList.get(i + 1));
+            }
+        }
+
+        return result;
+    }
+
+    public static Collection<Integer> findLocalMax(Collection<Integer> numbers) {
+        ArrayList<Integer> arrayList = (ArrayList<Integer>) numbers;
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i < arrayList.size() - 1; i++) {
+            if (arrayList.get(i) > arrayList.get(i - 1) && arrayList.get(i) > arrayList.get(i + 1)) {
+                result.add(arrayList.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean findSequence(Collection<Integer> numbers) {
+        ArrayList<Integer> arrayList = (ArrayList<Integer>) numbers;
+
+        for (int i = 1; i <= arrayList.size(); i++) {
+            for (int j = 0; j < arrayList.size(); j++) {
+                if (arrayList.get(j) != i) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static String findSimilar(Collection<String> names) {
+        ArrayList<String> arrayList = (ArrayList<String>) names;
+
+        String name = "";
+        int num = 0;
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            String str = arrayList.get(i);
+            int count = 0;
+            for (int j = 0; j < arrayList.size(); j++) {
+                if (arrayList.get(i).equals(arrayList.get(j))) {
+                    count++;
+                }
+            }
+            if (!str.equals(name) && num < count) {
+                name = str;
+                num = count;
+            }
+        }
+
+        return name + " : " + num;
+    }
+}
