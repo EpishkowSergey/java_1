ced800c12f07ec6f4cc26194ef61529567c8779d
Sergey <epishkow_sergey@mail.ru>
2020-07-07 14:07:29 +0300

    H11 Домашнее задание. Задача 2. Исправил метод findSimilar.

--- src/ru/progwards/java1/lessons/collections/Finder.java
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -62,7 +62,7 @@ public class Finder {
     public static String findSimilar(Collection<String> names) {
         ArrayList<String> arrayList = (ArrayList<String>) names;
 
-        String name = "";
+        String name = arrayList.get(0);
         int num = 0;
 
         for (int i = 0; i < arrayList.size(); i++) {
@@ -78,7 +78,7 @@ public class Finder {
                 }
             }
 
-            if (!str.equals(name) && num < count) {
+            if (str.equals(name) && num < count) {
                 name = str;
                 num = count;
             }
